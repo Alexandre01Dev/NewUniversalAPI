47d987c519048abdac4d117f23aba1109300f34a
Alexandre01Dev <altata01@hotmail.com>
2022-05-10 21:18:46 +0200

    Folder commit

--- src/main/java/be/alexandre01/eloriamc/manager/RankManager.java
+++ src/main/java/be/alexandre01/eloriamc/manager/RankManager.java
@@ -5,8 +5,11 @@ import lombok.Getter;
 import me.TechsCode.UltraPermissions.UltraPermissions;
 import me.TechsCode.UltraPermissions.UltraPermissionsAPI;
 import me.TechsCode.UltraPermissions.storage.collection.GroupList;
+import me.TechsCode.UltraPermissions.storage.objects.Group;
 import me.TechsCode.UltraPermissions.storage.objects.User;
 import org.bukkit.entity.Player;
+
+import java.util.Optional;
 
 @AllArgsConstructor
 public class RankManager {
@@ -16,26 +19,30 @@ public class RankManager {
     private Player player;
 
     public String getRankPrefix() {
-        User user = ultraPermissionsAPI.getUsers().name(player.getName()).get();
-        GroupList it = user.getActiveGroups().bestToWorst();
+        Optional<User> user = ultraPermissionsAPI.getUsers().name(player.getName());
+        if(!user.isPresent()) return null;
+        GroupList it = user.get().getActiveGroups().bestToWorst();
         if(it.isEmpty()) return null;
-        return it.get(0).getPrefix().get() + " ";
+        Optional<String> prefix = it.get(0).getPrefix();
+        return prefix.map(s -> s + " ").orElse(null);
     }
 
     public String getRankSuffix() {
-        User user = ultraPermissionsAPI.getUsers().name(player.getName()).get();
-        GroupList it = user.getActiveGroups().bestToWorst();
+        Optional<User> user = ultraPermissionsAPI.getUsers().name(player.getName());
+        if(!user.isPresent()) return null;
+        GroupList it = user.get().getActiveGroups().bestToWorst();
         if(it.isEmpty()) return null;
-        return it.get(0).getSuffix().get() + " ";
+        Optional<String> prefix = it.get(0).getSuffix();
+        return prefix.map(s -> s + " ").orElse(null);
     }
 
     public String getPlayerPrefix() {
-        User user = ultraPermissionsAPI.getUsers().name(player.getName()).get();
-        return user.getPrefix().get() + " ";
+        Optional<User> user = ultraPermissionsAPI.getUsers().name(player.getName());
+        return user.map(value -> value.getPrefix().orElse(null) + " ").orElse(null);
     }
 
     public String getPlayerSuffix() {
-        User user = ultraPermissionsAPI.getUsers().name(player.getName()).get();
-        return user.getSuffix().get() + " ";
+        Optional<User> user = ultraPermissionsAPI.getUsers().name(player.getName());
+        return user.map(value -> value.getSuffix().orElse(null) + " ").orElse(null);
     }
 }
