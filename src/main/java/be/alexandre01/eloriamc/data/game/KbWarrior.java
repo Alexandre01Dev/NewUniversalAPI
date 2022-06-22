8fdfa4b7d41fcea71219cc87d68a6e931d9e4ff8
Alexandre01Dev <altata01@hotmail.com>
2022-06-22 14:30:10 +0200

    Merci Github Copipi

--- src/main/java/be/alexandre01/eloriamc/data/game/KbWarrior.java
+++ src/main/java/be/alexandre01/eloriamc/data/game/KbWarrior.java
@@ -30,4 +30,33 @@ public class KbWarrior extends Identifier {
     @Expose private int[] blockList = {0};
 
 
+    public void addBlock(Block block){
+        addBlock(block.getTypeId());
+    }
+    public void addBlock(int b){
+        int[] newBlockList = new int[blockList.length + 1];
+        for(int i = 0; i < blockList.length; i++){
+            newBlockList[i] = blockList[i];
+        }
+        newBlockList[blockList.length] = b;
+        blockList = newBlockList;
+    }
+
+    public void removeBlock(Block block){
+        removeBlock(block.getTypeId());
+    }
+
+    public void removeBlock(int b){
+        int[] newBlockList = new int[blockList.length - 1];
+        int j = 0;
+        for(int i = 0; i < blockList.length; i++){
+            if(blockList[i] != b){
+                newBlockList[j] = blockList[i];
+                j++;
+            }
+        }
+        blockList = newBlockList;
+    }
+
+
 }
