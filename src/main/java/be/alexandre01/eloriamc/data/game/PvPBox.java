88c99f696a6bdcacfb29a0429d84edb809c7d82d
BuildTools <unconfigured@null.spigotmc.org>
2022-07-10 18:50:59 +0200

    EloriaAPI

--- src/main/java/be/alexandre01/eloriamc/data/game/PvPBox.java
+++ src/main/java/be/alexandre01/eloriamc/data/game/PvPBox.java
@@ -1,11 +1,17 @@
 package be.alexandre01.eloriamc.data.game;
 
 import com.google.gson.annotations.Expose;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class PvPBox extends Identifier {
-
-    @Expose
-    private int kill = 0;
+    @Expose private int kill = 0;
     @Expose private int death = 0;
     @Expose private int bestKs = 0;
     @Expose private float elo = 0;
